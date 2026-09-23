5df034ebec7c5d84e97c29e896012065728a56c8
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-24 07:20:49 +0900

    style: simplify batched HIGH tag token extraction

--- backend/src/application/crypto_search_orchestration.rs
+++ backend/src/application/crypto_search_orchestration.rs
@@ -165,7 +165,11 @@ impl HighSearchProjectionService {
         }
         let (mut combined_tokens, key_version) =
             self.derive_terms(owner_partition, &combined_terms).await?;
-        let tag_token = has_tag.then(|| combined_tokens.pop()).flatten();
+        let tag_token = if has_tag {
+            combined_tokens.pop()
+        } else {
+            None
+        };
         let mut content_tokens = combined_tokens;
         content_tokens.sort_by(|left, right| left.value.cmp(&right.value));
 
